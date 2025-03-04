10c17c2abf9d823b30cb9be0b3cb8684fe9183d3
Nitash-Biswas <nitash.nb@gmail.com>
2025-03-04 17:25:57 +0530

    Added backend routes as constants in frontend

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,3 +1,8 @@
 export const BASE_URL = "http://localhost:8000";
 
 export const USERS_URL = `/users`;
+export const VIDEOS_URL = `/videos`;
+export const TWEET_URL = `/tweets`;
+export const LIKES_URL = `/likes`;
+export const PLAYLISTS_URL = `/playlists`;
+export const SUBSCRIPTION_URL = `/subscriptions`;
